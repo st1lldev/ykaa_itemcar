1f23a66404be27df49767a709c308626655f64e8
St1ll <st1llczeu@proton.me>
2026-04-28 20:32:51 +0200

    Revise vehicle config with default vehicle and cooldown
    Updated vehicle configuration to include default vehicle and cooldown settings.

--- config.lua
+++ config.lua
@@ -1,23 +1,9 @@
 Config = {}
 
-Config.Framework = "esx" -- esx/qb framework
-
 Config.Vehicle = {
-    Full = true, -- Full tuning
-    VehicleStore = 'G', -- Key to store the vehicle (G by default)
-
+    Full = true,
+    DespawnKey = 47, -- G key
 }
 
-Config.CarItems = {
-    ['Bf400'] = 'bf400',
-
-}
-
--- ["bf400"] = { -- Add this to ox_inventory - data - items.lua
---    label = "BF400",
---    weight = 10,
---    stack = true,
---    client = {
---        export = "ykaa_itemcar.useItemCar"
---    }
--- },
+Config.DefaultVehicle = 'adder'
+Config.Cooldown = 20
