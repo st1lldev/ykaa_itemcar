2b5b1812b00fa4cdad6888f72429efe0e805b2df
St1ll <st1llczeu@proton.me>
2026-04-28 20:33:43 +0200

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,82 +1,122 @@
 local spawnedVehicle = nil
-local currentItem = nil
 local isMonitoring = false
+local cooldown = false
 
-exports('useItemCar', function(data, slot)
-    local itemName = data?.item?.name or data?.name
-    if not itemName then return end
+RegisterCommand('veh', function()
+    if cooldown then
+        lib.notify({
+            title = 'Vehicle Spawn',
+            description = 'Wait before spawning another vehicle.',
+            type = 'error'
+        })
+        return
+    end
 
-    local vehicleModel = Config.CarItems[itemName] or itemName
-    
-    TriggerEvent('ykaa_itemcar:spawnVehicle', vehicleModel, itemName)
+    TriggerEvent('ykaa_itemcar:spawnVehicle')
 end)
 
-RegisterNetEvent('ykaa_itemcar:spawnVehicle', function(model, itemName)
-    local playerPed = PlayerPedId()
-    local coords = GetEntityCoords(playerPed)
-    local hash = GetHashKey(model)
+RegisterKeyMapping('veh', 'Spawn configured vehicle', 'keyboard', '')
 
+RegisterNetEvent('ykaa_itemcar:spawnVehicle', function()
+    local playerPed = PlayerPedId()
+
+    cooldown = true
+
+    CreateThread(function()
+        Wait(Config.Cooldown * 1000)
+        cooldown = false
+    end)
+
+    if spawnedVehicle and DoesEntityExist(spawnedVehicle) then
+        DeleteEntity(spawnedVehicle)
+        spawnedVehicle = nil
+    end
+
+    local coords = GetEntityCoords(playerPed)
+    local forward = GetEntityForwardVector(playerPed)
+
+    local spawnCoords = vector3(
+        coords.x + (forward.x * 4.0),
+        coords.y + (forward.y * 4.0),
+        coords.z
+    )
+
+    local heading = GetEntityHeading(playerPed)
+    local hash = joaat(Config.DefaultVehicle)
 
     RequestModel(hash)
-    while not HasModelLoaded(hash) do Wait(0) end
 
-    spawnedVehicle = CreateVehicle(hash, coords.x, coords.y, coords.z, GetEntityHeading(playerPed), true, false)
-    TaskWarpPedIntoVehicle(playerPed, spawnedVehicle, -1)
-    
-    currentItem = itemName
-    TriggerServerEvent('ykaa_itemcar:removeItem', itemName)
+    while not HasModelLoaded(hash) do
+        Wait(10)
+    end
+
+    spawnedVehicle = CreateVehicle(
+        hash,
+        spawnCoords.x,
+        spawnCoords.y,
+        spawnCoords.z,
+        heading,
+        true,
+        false
+    )
+
+    SetVehicleOnGroundProperly(spawnedVehicle)
+    SetEntityAsMissionEntity(spawnedVehicle, true, true)
+    SetPedIntoVehicle(playerPed, spawnedVehicle, -1)
 
     if Config.Vehicle.Full then
         SetVehicleModKit(spawnedVehicle, 0)
+
         SetVehicleMod(spawnedVehicle, 11, GetNumVehicleMods(spawnedVehicle, 11) - 1, false)
+        SetVehicleMod(spawnedVehicle, 12, GetNumVehicleMods(spawnedVehicle, 12) - 1, false)
         SetVehicleMod(spawnedVehicle, 13, GetNumVehicleMods(spawnedVehicle, 13) - 1, false)
+
         ToggleVehicleMod(spawnedVehicle, 18, true)
-        
-        SetVehicleHandlingFloat(spawnedVehicle, 'CHandlingData', 'fInitialDriveForce', 0.8)
-        SetVehicleHandlingFloat(spawnedVehicle, 'CHandlingData', 'fDriveInertia', 1.0)
-        SetVehicleEnginePowerMultiplier(spawnedVehicle, 20.0)
+        ToggleVehicleMod(spawnedVehicle, 22, true)
+
+        SetVehicleEngineOn(spawnedVehicle, true, true, false)
     end
+
+    SetModelAsNoLongerNeeded(hash)
+
+    lib.notify({
+        title = 'Vehicle Spawn',
+        description = 'Vehicle spawned successfully.',
+        type = 'success'
+    })
 
     StartVehicleMonitor()
 end)
 
 function StartVehicleMonitor()
-    if isMonitoring then return end
-    isMonitoring = true
-    
-    Citizen.CreateThread(function()
-        local textUiOpen = false
-        while spawnedVehicle ~= nil do
-            local sleep = 500
-            local playerPed = PlayerPedId()
-            
-            if IsPedInVehicle(playerPed, spawnedVehicle, false) then
-                sleep = 0
-                if not textUiOpen then
-                    exports.ox_lib:showTextUI('[G] - Return vehicle', {
-                        position = 'right-center',
-                        icon = 'car'
-                    })
-                    textUiOpen = true
-                end
+    if isMonitoring then
+        return
+    end
 
-                if IsControlJustReleased(0, 47) then
-                    TriggerServerEvent('ykaa_itemcar:returnItem', currentItem)
-                    DeleteVehicle(spawnedVehicle)
+    isMonitoring = true
+
+    CreateThread(function()
+        while spawnedVehicle and DoesEntityExist(spawnedVehicle) do
+            local playerPed = PlayerPedId()
+
+            if IsPedInVehicle(playerPed, spawnedVehicle, false) then
+                if IsControlJustReleased(0, Config.Vehicle.DespawnKey) then
+                    DeleteEntity(spawnedVehicle)
                     spawnedVehicle = nil
-                    currentItem = nil
-                    exports.ox_lib:hideTextUI()
-                    textUiOpen = false
-                    break 
-                end
-            else
-                if textUiOpen then
-                    exports.ox_lib:hideTextUI()
-                    textUiOpen = false
+
+                    lib.notify({
+                        title = 'Vehicle Spawn',
+                        description = 'Vehicle despawned.',
+                        type = 'inform'
+                    })
+
+                    break
                 end
             end
-            Wait(sleep)
+
+            Wait(0)
         end
+
         isMonitoring = false
     end)
 end
